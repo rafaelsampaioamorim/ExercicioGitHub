92c8ae80e725e4420f2d3b95879fb80a1c947ebf
rafaelsampaioamorim <83255431+rafaelsampaioamorim@users.noreply.github.com>
2021-05-01 15:39:43 -0300

    Codificação da Classe de Circunferência
    Realizada a implantação da codificação da Classe de Circunferência, juntamente com a implantação no MAIN

--- ExercicioGitHun/src/exerciciogithun/Circuferencia.java
+++ ExercicioGitHun/src/exerciciogithun/Circuferencia.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exerciciogithun;
 
-/**
- *
- * @author prraf
- */
 public class Circuferencia {
+    float raio;
     
+    void setRaio(float valor){
+        this.raio=valor;
+    }
+    
+    float getRaio(){
+        return this.raio;
+    }
+    
+    double retornarPrimetro(){
+        if (this.raio==0){
+            System.out.println("Favor informar o valor de Raio para Circunferência");
+        }
+        return raio*2* Math.PI;
+    }  
+ 
+    double retornarArea(){
+        if (this.raio==0){
+            System.out.println("Favor informar o valor de Raio para Circunferência");
+        } 
+        return Math.PI*(Math.pow(raio, 2)); //PI.R^2
+    }
 }
--- ExercicioGitHun/src/exerciciogithun/ExercicioGitHun.java
+++ ExercicioGitHun/src/exerciciogithun/ExercicioGitHun.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exerciciogithun;
 
-/**
- *
- * @author prraf
- */
+
 public class ExercicioGitHun {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Favor Informar abaixo o que deseja Fazer: "
+                + "\n[1]-Calcular area de uma circunferência"
+                + "\n[2]-Calcular Perímetro de uma circunferência");
+        //inserir as opções do triangulo
+        
+        
+        
+        
     }
     
 }
